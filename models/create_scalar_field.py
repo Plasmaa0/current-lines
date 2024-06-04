f0460a5d8eb0f5a71e3f220b09c39d62a2fec9a0
Andrey Kuprin <plasmaa0@gmail.com>
2024-06-04 23:09:54 +0300

    chore: убрал модельки в отдельную папку а то их много уже

---
+++ models/create_scalar_field.py
@@ -0,0 +1,23 @@
+index = 1
+go = False
+nodes = []
+with open('t1.msh', 'r') as f:
+    for line in f:
+        if '$Nodes' in line:
+            go = True
+            continue
+        if '$End' in line:
+            go = False
+        if go:
+            line = line.strip()
+            nodes.append(line)
+
+nodes.pop(0)
+# print(nodes)
+from math import sin
+
+for node in nodes:
+    # print(node)
+    id, x, y, z = node.split(' ')
+    y = float(y)
+    print(int(id), sin(y*20)*4, abs(sin(y*20)*4)+1, 0)
